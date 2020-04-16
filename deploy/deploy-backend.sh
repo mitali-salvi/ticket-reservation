81f1b83084669dc3a6a7376656cb7dc00bc4ebaa
Mitali Salvi <salvi.mi@northeastern.edu>
2020-04-16 18:10:34 -0400

    Changed deployment script

--- deploy/deploy-backend.sh
+++ deploy/deploy-backend.sh
@@ -16,7 +16,12 @@ read IMAGE_NAME
 echo "Enter Stripe Key"
 read STRIPE_KEY
 
-RDS_URL="jdbc:mysql://$(AWS_PROFILE=${ENV} aws rds describe-db-instances --db-instance-identifier mysql-database | jq -r '.DBInstances[0].Endpoint.Address'):3306/csye7374?useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&createDatabaseIfNotExist=true"
+RDS_URL="jdbc:mysql://$(AWS_PROFILE=${ENV} aws rds describe-db-instances --db-instance-identifier mysql-database | jq -r '.DBInstances[0].Endpoint.Address'):3306/csye7200?useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=EST&createDatabaseIfNotExist=true&allowPublicKeyRetrieval=true"
+echo "RDS URL: ${RDS_URL}"
+echo ""
+echo ""
+echo ""
+
 echo "Creating namespace on k8s cluster"
 
 kubectl create namespace api
@@ -31,6 +36,7 @@ kubectl create namespace api
 # ES_URL=$(kubectl get svc elasticsearch -o json -n api| jq -r '.status.loadBalancer.ingress[0].hostname')
 
 echo "Creating MySQL Secrets"
+
 echo "Enter MySQL username"
 read RDS_USER
 
@@ -40,10 +46,8 @@ read RDS_PASSWORD
 kubectl create secret generic credentials \
   --from-literal=user=${RDS_USER} \
   --from-literal=password=${RDS_PASSWORD} \
-#   --from-literal=elb=${ES_URL} \
-#   --from-literal=bucket=${S3_ARN} \
   --from-literal=host=${RDS_URL} \
-  --from-literal=host=${STRIPE_KEY} \
+  --from-literal=STRIPE_KEY=${STRIPE_KEY} \
   --from-literal=AWS_ACCESS_KEY_ID=${ACCESS_KEY} \
   --from-literal=AWS_SECRET_ACCESS_KEY=${SECRET_ACCESS} \
   -n api
@@ -82,5 +86,8 @@ kubectl expose deployment backend -n api  --type=LoadBalancer --port 8080 --targ
 
 # kubectl create clusterrolebinding jenkins-default --clusterrole=cluster-admin --serviceaccount=jenkins:default
 
-echo "RDS URL: ${RDS_URL}"
+sleep 20
+APP_URL=$(kubectl get svc backend -o json -n api | jq -r '.status.loadBalancer.ingress[0].hostname')
+
+echo "BACKEND URL: ${APP_URL}"
 # echo "Elasticsearch LoadBalancer: ${ES_URL}"
